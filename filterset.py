5d787116f244ab37a7fc5f1df8da97c10b6112d0
Stephen <subsume@gmail.com>
2010-02-01 21:42:43 +0000

    another fix re:previous

--- filterset.py
+++ filterset.py
@@ -47,24 +47,34 @@ class JoinsafeFilterSet(FilterSet):
 
 			for i in tables:
 				if not i in redux_table_map[table_name]:
-					removed_tables[i] = table_name #new_table_map[table_name][-1]
+					if not i in removed_tables:
+						removed_tables[i] = []
+						qs.query.unref_alias(i)
+					removed_tables[i].append(table_name)
 
 		redux_join_map = {}
 
 		for join_tuple, tables in qs.query.join_map.iteritems():
 			redux_join_map[join_tuple] = [t for t in tables if not t in removed_tables]
-
+		
 		redux_alias_map = {}
 
 		for k, v in qs.query.alias_map.iteritems():
 			if not k in removed_tables:
 				redux_alias_map[k] = v
 
-		qs.query.table_map = redux_table_map
-		qs.query.join_map = redux_join_map
-		qs.query.alias_map = redux_alias_map
-		for key, value in removed_tables.iteritems():
-			qs.query.unref_alias(key)
+			if v[3] in removed_tables:
+				"""
+				Damn you ORM.
+				"""
+				redux_alias_map[k] = (v[0],v[1],v[2],removed_tables[v[3]][0],v[4],v[5],v[6])
 
-		qs.query.where.relabel_aliases(removed_tables)
+		qs.query.table_map = redux_table_map
+		#qs.query.join_map = redux_join_map
+		qs.query.alias_map = redux_alias_map
+			
+		for key, value in removed_tables.iteritems():
+			for v in value:
+				qs.query.where.relabel_aliases({key: v})
+
 		return qs
